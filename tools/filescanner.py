21d41154f60a880c7150051efac3952faedc1834
silviu <silviu.cacoveanu@hp.com>
2012-07-05 16:01:23 +0300

    simple file scanner tool

---
+++ tools/filescanner.py
@@ -0,0 +1,18 @@
+'''
+Created on Jul 5, 2012
+
+@author: cacovean
+'''
+
+import os, glob
+
+startFolder = 'C:\Users\cacovean\AppData\Roaming\Microsoft\Windows\Start Menu\Programs'
+startFolder2 = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs'
+
+os.chdir(startFolder)
+fileList = glob.glob("*.*")
+print fileList
+
+os.chdir(startFolder2)
+fileList = glob.glob("*.*")
+print fileList
